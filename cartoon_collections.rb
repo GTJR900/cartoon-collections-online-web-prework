905b862b8a2f962d5f2695b9f07cd04289e424f6
Gregory Thomas <gregorythomasjr55@gmail.com>
2019-06-12 16:10:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,3 +6,11 @@ end
 
 roll_call_dwarves(dwarves)
 
+planeteer_calls = %w[earth wind fire water heart]
+
+# Expected return value: ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end
+
+summon_captain_planet(planeteer_calls)
